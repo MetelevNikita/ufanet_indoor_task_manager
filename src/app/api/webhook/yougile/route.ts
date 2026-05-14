a02d9d44d2a1a042541d8f0969d7108d9f9ed3b4
Никита А. Метелёв <kyle.b@mail.ru>
2026-05-14 11:06:03 +0500

    fix YG webhook

--- src/app/api/webhook/yougile/route.ts
+++ src/app/api/webhook/yougile/route.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import { telegramBot } from '@/lib/telegramBot'
 
 // webhooks FN
 
@@ -9,6 +10,9 @@ import { getYGUsers } from "@/functions/yougileFnWebhook/getYGUsers";
 export const POST = async (req: NextRequest) => {
     try {
 
+
+        const bot = await telegramBot()
+
         const data = await req.json()
 
         // 
@@ -17,137 +21,137 @@ export const POST = async (req: NextRequest) => {
 
         let message;
 
-        // if (data.event === 'task-created') {
-        //     console.log('Проверка на создания новой карточки')
-        //     const columnTask = await getYGColumns(data.payload.columnId, key, url)
+        if (data.event === 'task-created') {
+            console.log('Проверка на создания новой карточки')
+            const columnTask = await getYGColumns(data.payload.columnId, key, url)
 
-        //     console.log(`Карточка ${data.payload.title} создана и перемещена в колонку ${columnTask.data.title as string}`)
-        //     message = `Карточка ${data.payload.title} создана и перемещена в колонку ${columnTask.data.title as string}`
+            console.log(`Карточка ${data.payload.title} создана и перемещена в колонку ${columnTask.data.title as string}`)
+            message = `Карточка ${data.payload.title} создана и перемещена в колонку ${columnTask.data.title as string}`
 
-        //     //  Отправка пользователю 
-
-
-
-        //     // 
+            //  Отправка пользователю 
 
 
-        //     return NextResponse.json({
-        //         success: true,
-        //         message: 'Карточка создана',
-        //         data: message
-        //     })
-        // }
+
+            // 
+
+
+            return NextResponse.json({
+                success: true,
+                message: 'Карточка создана',
+                data: message
+            })
+        }
         
-        // // move
+        // move
 
-        // if (data.event === 'task-moved') {
+        if (data.event === 'task-moved') {
 
-        //     console.log('Проверяю статуст перемещения задачи')
+            console.log('Проверяю статуст перемещения задачи')
 
-        //     if (JSON.stringify(data.payload.columnId) === JSON.stringify(data.prevData.columnId)) {
-        //     console.log('Карточка осталась на текущей доске')
-        //     return null
-        //     } else {
+            if (JSON.stringify(data.payload.columnId) === JSON.stringify(data.prevData.columnId)) {
+            console.log('Карточка осталась на текущей доске')
+            return null
+            } else {
 
-        //     const newColumn = await getYGColumns(data.payload.columnId, key, url)
+            const newColumn = await getYGColumns(data.payload.columnId, key, url)
 
-        //     if (newColumn.data.title === 'Входящие' || newColumn.data.title === 'Согласовано' || newColumn.data.title === 'Отклонено') {
-        //     console.log('Данная колонка попадает в исключение')
-        //     return null
-        //     }
+            if (newColumn.data.title === 'Входящие' || newColumn.data.title === 'Согласовано' || newColumn.data.title === 'Отклонено') {
+            console.log('Данная колонка попадает в исключение')
+            return null
+            }
 
-        //     if (!newColumn.data) return newColumn.message
+            if (!newColumn.data) return newColumn.message
 
-        //     console.log(`Карточка ${data.payload.title} перемещена новая доска ${newColumn.data.title as string}`)
-        //     message = `Карточка ${data.payload.title} перемещена новая доска ${newColumn.data.title as string}`
+            console.log(`Карточка ${data.payload.title} перемещена - новая доска ${newColumn.data.title as string}`)
+            message = `Карточка ${data.payload.title} перемещена - новая доска ${newColumn.data.title as string}`
 
-        //     //  Отправка пользователю 
+            //  Отправка пользователю 
 
+            await bot.sendMessage('-5141005635', message)
 
-
-        //     // 
-
-
-        //     return NextResponse.json({
-        //         success: true,
-        //         message: 'Карточка перемещана',
-        //         data: message
-        //     })
-        // }
-
-        // }
-
-        // // user
-
-        // if (data.event === 'task-updated') {
-
-        //     const prevUser = data.prevData.assigned ?? null
-        //     const payloadUser = data.payload.assigned ?? null
+            // 
 
 
-        //     if (JSON.stringify(prevUser) === JSON.stringify(payloadUser)) {
-        //         console.log('Данные о пользователях не изменились')
-        //         return null
-        //     } else {
-        //     const newUser = await getYGUsers(payloadUser, key, url)
+            return NextResponse.json({
+                success: true,
+                message: 'Карточка перемещана',
+                data: message
+            })
+        }
 
-        //     if (!payloadUser) return newUser.message
+        }
+
+        // user
+
+        if (data.event === 'task-updated') {
+
+            const prevUser = data.prevData.assigned ?? null
+            const payloadUser = data.payload.assigned ?? null
+
+
+            if (JSON.stringify(prevUser) === JSON.stringify(payloadUser)) {
+                console.log('Данные о пользователях не изменились')
+                return null
+            } else {
+            const newUser = await getYGUsers(payloadUser, key, url)
+
+            if (!payloadUser) return newUser.message
         
-        //     console.log(`Назначен новый испольнитель(и) - получаю новый список ${(!payloadUser) ? "СПИСОК ПУСТ" : JSON.stringify(newUser.data.map((item: any) => item.name).join(','))}`)
-        //     message = `Назначен новый испольнитель(и) - получаю новый список ${(!payloadUser) ? "СПИСОК ПУСТ" : JSON.stringify(newUser.data.map((item: any) => item.name).join(','))}`
+            console.log(`Назначен новый испольнитель(и) - получаю новый список ${(!payloadUser) ? "СПИСОК ПУСТ" : JSON.stringify(newUser.data.map((item: any) => item.name).join(','))}`)
+            message = `Назначен новый испольнитель(и) - получаю новый список ${(!payloadUser) ? "СПИСОК ПУСТ" : JSON.stringify(newUser.data.map((item: any) => item.name).join(','))}`
 
-        //     //  Отправка пользователю 
+            //  Отправка пользователю 
 
+              await bot.sendMessage('-5141005635', message)
 
+            // 
 
-        //     // 
-
-        //     return NextResponse.json({
-        //         success: true,
-        //         message: 'Изменен исполнитель в карточке',
-        //         data: message
-        //     })
-        // }
-
-
-        // }
-
-        // // sticker sprint
-
-        // if (data.event === 'task-updated') {
-
-        //     const prevSticker = data.prevData.stickers ?? null
-        //     const payloadSticker = data.payload.stickers ?? null
+            return NextResponse.json({
+                success: true,
+                message: 'Изменен исполнитель в карточке',
+                data: message
+            })
+        }
 
 
-        //     if (JSON.stringify(prevSticker) === JSON.stringify(payloadSticker)) {
-        //         console.log('Данные стикеров не изменились')
-        //         return null
-        // } else {
-        //     const sticker = await getYGStickerSprint(payloadSticker, key, url)
-        //     console.log(sticker)
+        }
 
-        //     if (!sticker.data) return sticker.message
+        // sticker sprint
 
-        //     const messageFromSticeker = sticker.data.map(async (item: {steickerName: string, currentState: {name: string, color: string}}): Promise<any> => {
-        //         return `${item.steickerName} стикер изменен, новое состояние - ${item.currentState.name}`
-        //     })
+        if (data.event === 'task-updated') {
 
-        //     //  Отправка пользователю 
+            const prevSticker = data.prevData.stickers ?? null
+            const payloadSticker = data.payload.stickers ?? null
 
 
+            if (JSON.stringify(prevSticker) === JSON.stringify(payloadSticker)) {
+                console.log('Данные стикеров не изменились')
+                return null
+        } else {
+            const sticker = await getYGStickerSprint(payloadSticker, key, url)
+            console.log(sticker)
 
-        //     // 
+            if (!sticker.data) return sticker.message
 
-        //     return NextResponse.json({
-        //         success: true,
-        //         message: 'Изменен стикер в карточке',
-        //         data: messageFromSticeker.join(', ')
-        //     })
-        // }
+            const messageFromSticeker = sticker.data.map(async (item: {steickerName: string, currentState: {name: string, color: string}}): Promise<any> => {
+                return `${item.steickerName} стикер изменен, новое состояние - ${item.currentState.name}`
+            })
+
+            //  Отправка пользователю 
+
+            await bot.sendMessage('-5141005635', messageFromSticeker)
+
+            // 
+
+            return NextResponse.json({
+                success: true,
+                message: 'Изменен стикер в карточке',
+                data: messageFromSticeker.join(', ')
+            })
+        }
 
 
-        // }
+        }
 
 
         return NextResponse.json({
